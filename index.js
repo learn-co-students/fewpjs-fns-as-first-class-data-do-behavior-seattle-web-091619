31284f3e83bfd596626b25c321b21ec4256b4031
Packmanager9 <ColeJDitzler@gmail.com>
2019-10-29 13:06:15 -0700

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time_string) {
+  if (parseInt(timeparts[0]) < 17) {
+    let string = 'Good Afternoon'
+    } 
+  let timeparts = timestring.split()
+  if (parseInt(timeparts[0]) < 12) {
+  let string = 'Good Morning'
+  }
+    if (parseInt(timeparts[0]) > 17) {
+      let string = 'Good Evening'
+      }
+      return string
+}
+
+function displayMessage(textual){
+  box = document.getElementById('time')
+  box.textContent(textual)
+
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
